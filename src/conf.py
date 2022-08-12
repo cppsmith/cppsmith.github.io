f6315fa22d63a60af993479d7c10e0f6f823657b
Eugene Kuznetsov <eugene.kuznetsov.de@gmail.com>
2022-08-13 00:26:01 +0200

    feat: use Furo Sphinx theme

--- src/conf.py
+++ src/conf.py
@@ -1,8 +1,23 @@
-project = 'cppsmith.dev'
-copyright = '2022, Eugene Kuznetsov'
-author = 'Eugene Kuznetsov'
-extensions = []
+copyright = u'2022, Eugene Kuznetsov'
+extensions = [
+    'sphinxext.opengraph',
+    'sphinx_inline_tabs',
+    'sphinx_copybutton'
+]
 templates_path = []
 exclude_patterns = []
-html_theme = 'alabaster'
-html_static_path = []
+
+html_theme = 'furo'
+html_theme_options = {
+    'sidebar_hide_name': True,
+    'source_repository': 'https://github.com/cppsmith/cppsmith.github.io',
+    'source_branch': 'master',
+    'source_directory': 'src'
+}
+html_title = 'cppsmith'
+html_baseurl = 'https://cppsmith.dev'
+html_logo = '_static/cppsmith.png'
+html_static_path = ['_static']
+
+ogp_site_url = html_baseurl
+opg_image = html_logo
