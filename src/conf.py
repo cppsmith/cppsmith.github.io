5fa1fff595ea81eabb15b87065f1064c7fc6f8b5
Eugene Kuznetsov <eugene.kuznetsov.de@gmail.com>
2022-08-24 23:17:20 +0200

    feat: use Markdown for pages

--- src/conf.py
+++ src/conf.py
@@ -1,23 +1,14 @@
 copyright = u'2022, Eugene Kuznetsov'
-extensions = [
-    'sphinxext.opengraph',
-    'sphinx_inline_tabs',
-    'sphinx_copybutton'
-]
-templates_path = []
-exclude_patterns = []
+
+extensions = ['myst_parser']
+
+source_suffix = ['.md']
 
 html_theme = 'furo'
 html_theme_options = {
-    'sidebar_hide_name': True,
-    'source_repository': 'https://github.com/cppsmith/cppsmith.github.io',
-    'source_branch': 'master',
-    'source_directory': 'src'
+    'sidebar_hide_name': True
 }
-html_title = 'cppsmith'
+html_title = 'CPPsmith'
 html_baseurl = 'https://cppsmith.dev'
 html_logo = '_static/cppsmith.png'
 html_static_path = ['_static']
-
-ogp_site_url = html_baseurl
-opg_image = html_logo
---
+++ src/index.md
@@ -0,0 +1,3 @@
+# Hello there!
+
+My name is [Eugene Kuznetsov](https://www.linkedin.com/in/ekuznetsov/) and I am a passionate software engineer. This is my blog where you will find articles about my interests in software development. As C++Smith, I will try to "craft" articles and post them at least once per week, so stay tuned.
